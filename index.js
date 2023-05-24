1c53e6141195590307904814ec0478e9e81f38a4
Érika Soares <contacto@erikazdesign.com>
2023-05-24 17:49:55 +0100

    set up server

---
+++ index.js
@@ -0,0 +1,36 @@
+const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+
+const {MONGODB } = require("./config.js");
+
+const typeDefs = gql`
+  type Query {
+    sayHi: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHi: () => "Hello World!!!",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose
+.connect(MONGODB, {useNewUrlParser:true})
+.then(() => {
+    console.log("MongoDB connected!");
+    return server.listen({port:5000});
+})
+.then((res) => {
+    console.log(`Server running at ${url}`)
+})
+
+server.listen({ port: 5000 }).then(({ url }) => {
+  console.log(`Server running at ${url}`);
+});
